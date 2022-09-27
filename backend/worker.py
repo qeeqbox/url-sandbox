66ac7fa338b02fea12f947afd66adc49803158f2
w3tmo <zchreichert@gmail.com>
2022-09-27 14:21:34 -0400

    Fixed typo in container name
    Changed "url-sandbox_box" to "url-sandbox-box" so Docker could find and run the correct container.

--- backend/worker.py
+++ backend/worker.py
@@ -34,7 +34,7 @@ CELERY.conf.update(
 
 def clean_up():
     for container in DOCKER_CLIENT.containers.list():
-        if "url-sandbox_box" in container.name:
+        if "url-sandbox-box" in container.name:
             container.stop()
 
 
@@ -54,10 +54,10 @@ def analyze_url(self, parsed):
         parsed['locations'] = json_settings[environ["project_env"]]["task_logs"]
         if parsed['use_proxy']:
             log_string("Proxy detected", task=parsed['task'])
-            temp_container = DOCKER_CLIENT.containers.run("url-sandbox_box", command=[hexlify(pdumps(parsed)).decode()], volumes={json_settings[environ["project_env"]]["output_folder"]: {'bind': json_settings[environ["project_env"]]["task_logs"]["box_output"], 'mode': 'rw'}}, detach=True, network="url-sandbox_frontend_box")
+            temp_container = DOCKER_CLIENT.containers.run("url-sandbox-box", command=[hexlify(pdumps(parsed)).decode()], volumes={json_settings[environ["project_env"]]["output_folder"]: {'bind': json_settings[environ["project_env"]]["task_logs"]["box_output"], 'mode': 'rw'}}, detach=True, network="url-sandbox_frontend_box")
         else:
             log_string("No proxy, running privileged for custom tor config", task=parsed['task'])
-            temp_container = DOCKER_CLIENT.containers.run("url-sandbox_box", command=[hexlify(pdumps(parsed)).decode()], volumes={json_settings[environ["project_env"]]["output_folder"]: {'bind': json_settings[environ["project_env"]]["task_logs"]["box_output"], 'mode': 'rw'}}, detach=True, network="url-sandbox_frontend_box", privileged=True)
+            temp_container = DOCKER_CLIENT.containers.run("url-sandbox-box", command=[hexlify(pdumps(parsed)).decode()], volumes={json_settings[environ["project_env"]]["output_folder"]: {'bind': json_settings[environ["project_env"]]["task_logs"]["box_output"], 'mode': 'rw'}}, detach=True, network="url-sandbox_frontend_box", privileged=True)
         temp_logs = ""
         for item in range(1, parsed['analyzer_timeout']):
             temp_logs = temp_container.logs()
